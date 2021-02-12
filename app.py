e63d9fbf547ea11d2b366be93ee415b9fb1026be
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2021-02-12 09:07:34 -0600

    Added Dotenv + addData endpoint

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+import csv
 import os
-from azure.storage.blob import BlobServiceClient
-
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
 from Database.DatasetDB import Dataset
+from Database.Engine import engine, Base
 from Database.UUID import UniqueIds
-from secret import *
 import pathlib
 from flask import Flask, request
 from flask_cors import CORS
 import uuid
+import pandas as pd
+import settings
 
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+Base.metadata.create_all(bind=engine)
 
 TEMP_FOLDER = 'tmp' + os.sep
 
@@ -61,13 +65,52 @@ def createProject():
     return {'fileName': path, 'success': True}
 
 
+@app.route('/addData', methods=['POST'])
+def addData():
+    fileID = request.json['fileID']
+    fileType = request.json['fileType']
+    columnsToAppend = request.json['columnsToAppend']
+
+    if fileType == '.csv':
+        blobServiceClient = BlobServiceClient.from_connection_string(os.getenv('AZURE_CONNECTION_STRING'))
+        containerName = "datasets"
+        blobClient = blobServiceClient.get_blob_client(container=containerName, blob=fileID + fileType)
+        downloadPath = 'tmp' + os.sep + fileID + fileType
+        folder = open(downloadPath, "w+")
+        folder.write(blobClient.download_blob().readall().decode("utf-8"))
+        folder.close()
+
+        rowsToAppend = []
+        with open(downloadPath, newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                rowsToAppend.append(row)
+
+        f = open(downloadPath, "w+")
+        f.close()
+
+        rowsToAppend.append(columnsToAppend)
+        with open(downloadPath, "a", encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerows(rowsToAppend)
+
+        df = pd.read_csv(downloadPath)
+        df.to_csv(downloadPath, index=False)
+
+        uploadFile(downloadPath)
+
+        filePath = pathlib.Path(downloadPath)
+        filePath.unlink()
+    return {'success': True}
+
+
 def uploadFile(filePath):
-    blobServiceClient = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
+    blobServiceClient = BlobServiceClient.from_connection_string(os.getenv('AZURE_CONNECTION_STRING'))
     containerName = "datasets"
     blobClient = blobServiceClient.get_blob_client(container=containerName, blob=filePath.replace('tmp' + os.sep, ''))
 
     with open(filePath, "rb") as data:
-        blobClient.upload_blob(data)
+        blobClient.upload_blob(data, overwrite=True)
 
 
 if __name__ == '__main__':
--- app_tests.py
+++ app_tests.py
@@ -1,8 +1,11 @@
+import os
+
 import requests
-from secret import *
+import settings
 
 pload = {'fileType': '.csv',
-         'columns': ['Hello', 'World']}
+         'fileID': '8f91ac96-f2c7-4f8d-981b-90d77a4b7236',
+         'columnsToAppend': ['How', 'is Life']}
 
-r = requests.post(LOCAL + 'createProject', json=pload)
+r = requests.post(os.getenv('LOCAL') + 'addData', json=pload)
 print(r.content)
