51ce524a7fc2de7d7adddcbfc973e71a7351dbcc
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2021-04-18 02:25:43 -0500

    aws db

--- Database/Engine.py
+++ Database/Engine.py
@@ -9,8 +9,8 @@ import settings
 # Sqlite will just come from a file
 package_dir = os.path.abspath(os.path.dirname(__file__))
 dbPath = os.path.join(package_dir, 'test.db')
-engine = create_engine('sqlite:///' + dbPath, connect_args={'check_same_thread': False})
-# engine = create_engine('mysql+mysqlconnector://' + os.getenv('DB_URL'))
+# engine = create_engine('sqlite:///' + dbPath, connect_args={'check_same_thread': False})
+engine = create_engine('mysql+mysqlconnector://' + os.getenv('DB_URL'))
 dbSession = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = dbSession.query_property()
